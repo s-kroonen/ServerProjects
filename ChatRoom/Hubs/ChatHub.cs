43167918c2bda18307cdcd9faa4fae87da510778
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-04-17 20:25:30 +0200

    added light function

--- ChatRoom/Hubs/ChatHub.cs
+++ ChatRoom/Hubs/ChatHub.cs
@@ -1,18 +1,38 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChatRoom.Models;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Concurrent;
 
 public class ChatHub : Hub
 {
-    private static readonly List<(string User, string Text)> _messages = new();
+    #region messages
+    private static readonly List<Message> _messages = new();
 
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(string user, string content)
     {
-        _messages.Add((user, message));
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        var message = new Message { User = user, Content = content };
+        _messages.Add(message);
+        await Clients.All.SendAsync("ReceiveMessage", message);
     }
 
-    public Task<List<(string User, string Text)>> GetMessageHistory()
+    public Task<List<Message>>GetMessageHistory()
     {
-        return Task.FromResult(_messages.ToList());
+        return Task.FromResult(_messages);
     }
+    #endregion
+
+    private static int LightBrightness = 100; // default full brightness
+    private static string CurrentController = null;
+    public Task<string> GetCurrentController() => Task.FromResult(CurrentController);
+
+    public Task<int> GetBrightness() => Task.FromResult(LightBrightness);
+
+    public async Task SetBrightness(string username, int brightness)
+    {
+        if (CurrentController == null || CurrentController == username)
+        {
+            LightBrightness = Math.Clamp(brightness, 0, 100);
+            await Clients.All.SendAsync("BrightnessChanged", LightBrightness, username);
+        }
+    }
+
 }
--- ChatRoom/obj/Debug/net9.0/ChatRoom.AssemblyInfo.cs
+++ ChatRoom/obj/Debug/net9.0/ChatRoom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatRoom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56acab25289513ccdab4768dce820ff455d7dc08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatRoom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatRoom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

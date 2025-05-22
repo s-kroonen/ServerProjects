2afc86d703b5cc79392077b3b7f391db9faf3f3e
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-05-22 10:02:02 +0200

    Orm testing init

---
+++ ORM-testing/LitterDbContex.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ORM_testing
+{
+    public class WeatherForecastDbContex : DbContext
+    {
+        public WeatherForecastDbContex(DbContextOptions<WeatherForecastDbContex> options) : base(options) { }
+        public DbSet<WeatherForecast> weatherForecasts { get; set; }
+    }
+}
